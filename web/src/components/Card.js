b4c42016caeb58ad8e3903a181f66d8e476aefd2
J-Souffle <jd.cancio1@gmail.com>
2023-10-08 07:52:24 -0400

    Tried using Modal and Popup but doesn't work.

--- web/src/components/Card.js
+++ web/src/components/Card.js
@@ -3,7 +3,7 @@ import './Card.css'
 import Popup from '../components/Popup';
 import { useState } from 'react';
 
-function Card({title,imgUrl,body}) {
+function Card({title,imgUrl,body,desc}) {
     const [buttonPopup, setButtonPopup] = useState(false);
     return (
         <div className='card-container'>
@@ -20,12 +20,13 @@ function Card({title,imgUrl,body}) {
             </div>
             
             <div className="btn">
-            <button onClick={() => setButtonPopup(true)}>Open Popup</button>
-        
+            <button onClick={() => setButtonPopup(true)}>View Desc</button>
+        <main>
         <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
                 <h3>My popup</h3>
                 <p>This is my button triggered popup</p>
             </Popup>
+            </main>
             </div>
         </div>
     )
--- web/src/pages/Products.jsx
+++ web/src/pages/Products.jsx
@@ -1,23 +1,15 @@
 import Footer from "../components/Footer";
 import "nes.css/css/nes.min.css";
 import Card from '../components/Card'
-import Popup from '../components/Popup';
-import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+
 
 
 export const Products = () => {
-    const [buttonPopup, setButtonPopup] = useState(false);
+
     return (
         <>
-        <main>
-            <h1>React Popups</h1>
-            <br></br>
-            <button onClick={() => setButtonPopup(true)}>Open Popup</button>
-        </main>
-        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-                <h3>My popup</h3>
-                <p>This is my button triggered popup</p>
-            </Popup>
+        <Modal />
             <div className="nes-container with-title shopTitle">
             <h1>Products</h1>
         </div>
