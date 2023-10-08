53e951a7a16a08f62f87a9baf6a74bff61cacae9
J-Souffle <jd.cancio1@gmail.com>
2023-10-08 05:37:57 -0400

    Implemented a card component and modified product.

--- web/src/pages/Products.jsx
+++ web/src/pages/Products.jsx
@@ -1,10 +1,25 @@
-import { useEffect } from 'react';
+import Footer from "../components/Footer";
+import "nes.css/css/nes.min.css";
+import Card from '../components/Card'
+
 
 export const Products = () => {
 
     return (
         <>
-            <h2>Products</h2>
+            <div className="nes-container with-title shopTitle">
+            <h1>NoIdea's Webscraper</h1>
+        </div>
+        <br></br>
+        <div className="App">
+           
+                <Card 
+                title='Card Title'
+                imgUrl='https://upload.wikimedia.org/wikipedia/en/e/e7/World_Tennis_Championships.jpg'
+                body='test1'/>
+            </div>
         </>
     )
 }
+
+export default Products
